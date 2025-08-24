8fefe2daa5e5ca6beb96b4a84f7afc2812b994f4
devpsoni <iamdevsoni@gmail.com>
2025-08-24 10:56:41 +0530

    Initial Commit

---
+++ Recipe Search/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe Search
+//
+//  Created by Dev Soni on 24/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe Search/Recipe_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_SearchApp.swift
+//  Recipe Search
+//
+//  Created by Dev Soni on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
